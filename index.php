1c9955d5952432dac0127563bea52b95abef8b80
malubimcev <malubimcev@gmail.com>
2018-04-03 13:27:27 +0300

    not ready

--- index.php
+++ index.php
@@ -3,35 +3,53 @@
     <head>
         <meta charset="windows-1251">
         <title>PHP_Lesson_3</title>
+        <link rel="stylesheet" href="styles.css"/>
     </head>
     <body>
-        <h1>Animals</h1>
+      <h1>PHP Lesson 3. Animals</h1>
+      <div class="main-container">
+        <div class="inner-container-1">
         <?php
-            $continents = [];
-            $animals = array(
-                "Africa" => array("Crocodylus Niloticus", "Pan Troglodytes", "Hippopotamus Amphibius", "Giraffa Camelopardalis"),
-                "Australia" => array("Phascolarctos Cinereus", "Ornithorhynchus Anatinus", "Sarcophilus Harrisii"),
-                "Antarctica" => array("Pygoscelis adeliae", "Hydrurga Leptonyx"),
-                "America" => array("Dasypus Novemcinctus", "Lama Guanicoe", "Eunectes Murinus"),
-                "Asia" => array("Phodopus Sungorus", "Panthera Tigris Altaica", "Vulpes Lagopus")
-            );
-            foreach ($animal as $animals => $value) {
-                
-            }
-
-            do {
-                if ($y1 > $x) {
-                    $message = 'is not in Fibonacci Sequence';
-                } elseif ($y1 == $x) {
-                    $message = 'is in Fibonacci Sequence';
-                } else {
-                    $y3 = $y1;
-                    $y1 += $y2;
-                    $y2 = $y3;
+            $continents = [
+                "Africa" => ["Crocodylus Niloticus", "Pan Troglodytes", "Hippopotamus Amphibius", "Giraffa Camelopardalis"],
+                "Australia" => ["Phascolarctos Cinereus", "Ornithorhynchus Anatinus", "Sarcophilus Harrisii"],
+                "Antarctica" => ["Pygoscelis Adeliae", "Hydrurga Leptonyx"],
+                "America" => ["Dasypus Novemcinctus", "Lama Guanicoe", "Eunectes Murinus"],
+                "Asia" => ["Phodopus Sungorus", "Panthera Tigris Altaica", "Vulpes Lagopus"]
+            ];
+            $animalNames = [];
+            $firstWords = [];
+            $secondWords = [];
+            echo "<h2>Real Animals</h2>";
+            foreach ($continents as $continent => $animals) {
+                echo "<ul><h3>$continent</h3>";
+                foreach ($animals as $key => $animal) {
+                    echo "<li>$animal</li>";
+                    if (sizeof(explode(' ', $animal)) == 2) {
+                        $animalNames[][] = array($animal => $continent);
+                        $firstWords[][] = array($continent => explode(' ', $animal)[0]);
+                        $secondWords[] = array(explode(' ', $animal)[1]);
+                    }
                 }
-            } while ($message == '');
-            $message = 'x='.$x.' '.$message;
+                echo "</ul>";
+            }
         ?>
-        <p><?php echo $message ?></p>
+        </div>
+        <div class="inner-container-2">
+
+        <?php
+            $b = shuffle($secondWords);
+            var_dump($animalNames);echo "<br>";
+            foreach ($firstWords as $continent => $firstWord) {
+                echo "<ul><h2>$continent</h2>";
+                echo $firstWord[0][0];
+                //for ($i = 0; $i<sizeof($secondWords); $i++) {
+                    //echo "<li>$firstWord</li>";//$secondWord[$i]
+                //}
+                echo "</ul>";
+            }
+            echo "</div>";
+        ?>
+      </div>
     </body>
 </html>
