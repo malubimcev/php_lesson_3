6e7135590f00a1802550d8178f13633f7b27bbcd
malubimcev <malubimcev@gmail.com>
2018-04-06 21:37:52 +0300

    PHP_lesson_3. version 3

--- index.php
+++ index.php
@@ -1,15 +1,55 @@
 <?php
     //Составляем исходный массив реальных животных
-    $continents = [
-        "Africa" => ["Crocodylus Niloticus", "Pan Troglodytes", "Hippopotamus Amphibius", "Giraffa Camelopardalis"],
-        "Australia" => ["Phascolarctos Cinereus", "Ornithorhynchus Anatinus", "Sarcophilus Harrisii"],
-        "Antarctica" => ["Pygoscelis Adeliae", "Hydrurga Leptonyx"],
-        "America" => ["Dasypus Novemcinctus", "Lama Guanicoe", "Eunectes Murinus"],
-        "Asia" => ["Phodopus Sungorus", "Panthera Tigris Altaica", "Vulpes Lagopus"]
+    $animalsByContinent = [
+        "Africa" => [
+            "Crocodylus Niloticus",
+            "Pan Troglodytes",
+            "Hippopotamus Amphibius",
+            "Giraffa Camelopardalis"
+        ],
+        "Australia" => [
+            "Phascolarctos Cinereus",
+            "Ornithorhynchus Anatinus",
+            "Sarcophilus Harrisii"
+        ],
+        "Antarctica" => [
+            "Pygoscelis Adeliae",
+            "Hydrurga Leptonyx"
+        ],
+        "America" => [
+            "Dasypus Novemcinctus",
+            "Lama Guanicoe",
+            "Eunectes Murinus"
+        ],
+        "Asia" => [
+            "Phodopus Sungorus",
+            "Panthera Tigris Altaica",
+            "Vulpes Lagopus"
+        ]
     ];
     $animalNames = [];
     $secondWords = [];
     $currentName = [];
+    $newNames = [];
+    foreach ($animalsByContinent as $continent => $animals) {
+        foreach ($animals as $key => $animal) {
+            $currentName = explode(' ', $animal);
+            if (sizeof($currentName) == 2) {
+                //составляем новый массив животных с названиями из 2-х слов
+                $animalNames[] = array(
+                    'first_word' => $currentName[0],
+                    'second_word' => $currentName[1],
+                    'cont' => $continent
+                );
+            }
+        }
+    }
+    unset($currentName);
+    $secondWords = array_column($animalNames, 'second_word');//создаем отдельный массив для вторых слов названий
+    shuffle($secondWords);//перемешиваем вторые слова названий в массиве
+    for ($i = 0; $i < sizeof($animalNames); $i++) {
+        $newNames[] = implode(" ", array($animalNames[$i]["first_word"], $secondWords[$i], $animalNames[$i]['cont']));
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,52 +59,13 @@
         <link rel="stylesheet" href="styles.css"/>
     </head>
     <body>
-      <div class="main-container">
-        <!--<div class="inner-container-1">-->
+        <div class="inner-container">
         <?php
-            //echo "<h1>Real Animals</h1>";
-            foreach ($continents as $continent => $animals) {
-                //echo "<ul><h2>$continent</h2>";
-                foreach ($animals as $key => $animal) {
-                    //echo "<li>$animal</li>";
-                    $currentName = explode(' ', $animal);
-                    if (sizeof($currentName) == 2) {
-                        //составляем новый массив животных с названиями из 2-х слов
-                        $animalNames[] = array('first_word' => $currentName[0], 'second_word' => $currentName[1], 'cont' => $continent);
-                        //вторые части названий заносим в еще один массив для дальнейшей работы
-                        $secondWords[] = array('new_name' => $currentName[1]);
-                    }
-                }
-                echo "</ul>";
-            }
-            echo "<br>";
-        ?>
-        <!--</div>-->
-        <div class="inner-container-2">
-        <?php
-            //перемешиваем вторые слова названий в массиве
-            shuffle($secondWords);
             //вывод на экран
             echo "<h1>Fantastic Animals</h1>";
-            $continent = "";
-            $newName = "";//строка с новыми названиями животных
-            for ($i = 0; $i < sizeof($animalNames); $i++) {
-                if ($animalNames[$i]["cont"] === $continent) {
-                    //формирование строки с вымышленными названиями
-                    $newName = $newName.", ".$animalNames[$i]["first_word"]." ".$secondWords[$i]["new_name"];
-                } else {
-                    if ($i > 0) {
-                        echo $newName."<br>";
-                    }
-                    $continent = $animalNames[$i]["cont"];
-                    echo "<h2>$continent</h2><br>";
-                    $newName = $animalNames[$i]["first_word"]." ".$secondWords[$i]["new_name"];
-                }
-            }
-            echo $newName."<br>";
-            echo "<br>";
+            $listOfAnimals = str_replace(",", ", ", implode(",", $newNames));
+            echo "<p>"; echo $listOfAnimals; echo "</p>";
         ?>
         </div>
-      </div>
     </body>
 </html>
